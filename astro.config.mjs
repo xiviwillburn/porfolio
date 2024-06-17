26ee737b35a64c2e87cfec2068f7f150819a72b7
xiviwillburn <45106237+xiviwillburn@users.noreply.github.com>
2024-06-17 18:07:26 +0200

    fixed typo

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
   site: `xiviwillburn.github.io`,
   base: 'portfolio',
   output: 'static',
-  outDir: './docs',
+  outDir: './dist',
   build: {
     assets: 'astro'
   }
